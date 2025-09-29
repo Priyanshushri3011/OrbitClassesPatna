0585d5d8115f740ebc073cf6b6a4028b0972d063
Priyanshushri3011 <priyanshushri3011@gmail.com>
2025-09-29 17:25:31 +0530

    First commit

---
+++ src/Components/Hero/Welcome.jsx
@@ -0,0 +1,12 @@
+import wlcmimg from "../../assets/wlcmimg.png";
+function Welcome (){
+    return (
+        <>
+        <div className="wlcmain">
+            <img className="wlcmimg" src={wlcmimg} alt="" />
+
+        </div>
+        </>
+    )
+}
+export default Welcome;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import './Navbar.css';
+import logo from "../../assets/logo.png"
+import { FaYoutube } from "react-icons/fa";
+function Navbar() {
+    return (
+        <>
+            <div className="main">
+                <div className="logo">
+                    <img className='logoimg' src={logo} alt="logo" />
+
+                </div>
+                <div className="Allbuttons">
+                    <button>Home</button>
+                    <button>AboutUs</button>
+                    <button>Courses</button>
+                    <button>Events</button>
+
+                </div>
+                <div className="socialmedia">
+                    <a
+                        href="https://www.youtube.com/@Orbitclassespatna"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FaYoutube size={60} color="red" />
+                    </a>
+
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default Navbar;
+
+
